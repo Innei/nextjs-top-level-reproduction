dd4ea36444c8d30b4815bb1aed85f499d186570b
Innei <i@innei.in>
2024-03-03 00:52:05 +0800

    init
    Signed-off-by: Innei <i@innei.in>

---
+++ components/shiki.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+import type { FC } from 'react'
+import { highlighter } from './utils'
+
+export const ShikiHighLighter: FC<{}> = (props) => {
+  console.log(highlighter)
+  return null
+}
